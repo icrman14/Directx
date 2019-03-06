944d174318e9b395eda9fb07cc9d13a4951a2e29
icrman14 <38091743+icrman14@users.noreply.github.com>
2019-03-06 09:44:57 -0700

    Add files via upload
    Use this one

--- Source.cpp
+++ Source.cpp
@@ -7,134 +7,121 @@
 #include <string>
 
 
-struct Vec3
+std::string MarkerName(int num)
 {
-	Vec3 operator+( const Vec3& _other )const
-	{
-		return { x + _other.x, y + _other.y, z + _other.z };
-	}
-	Vec3 operator*( const double _scalar )const
-	{
-		return{ x * _scalar, y * _scalar, z * _scalar };
-	}
-	double x = 0., y = 0., z = 0.;
-};
-
-std::ostream& operator<<( std::ostream& stream, const Vec3& v )
-{
-	stream << v.x << " " << v.y << " " << v.z << '\n';
+	std::string str;
+	str = "marker.";
+	str.append(std::to_string(num));
+	return str;
 }
-std::istream& operator>>( std::istream& stream, Vec3& v )
-{
-	stream >> v.x >> v.y >> v.z;
-}
-std::string MakeHeader()
-{
-	return {
-		"TITLE=\"3D TRIANGULAR SURFACE DATA\"\n"
-		"VARIABLES= \"X\", \"Y\", \"Z\"\n"
-		"ZONE T=\"UNSTRUC\" N= 606 E= 804 F=FEPOINT ET=TRIANGLE\n"
-	};
-}
-std::vector<std::string> ExtractLines( std::string filename, int header_size, int line_count = -1 )
-{
-	std::ifstream file( filename );
-	assert( file.is_open() );
-	file.seekg( header_size );
 
-	std::vector<std::string> lines;
-	std::string line;
-
-	if(line_count == -1)
+void getPosition(std::vector<double> &x, std::vector<double> &y, std::vector<double> &z)
+{
+	std::ifstream file("unstruc_surface_in.dat");
+	assert(file.is_open());
+	file.seekg(10);
+	int num;
+	double xp;
+	double yp;
+	double zp;
+	int counter = 0;
+	file.precision(8);
+	while (file >> num >> xp >> yp >> zp)
 	{
-		while( std::getline( file, line ) )
-		{
-			if( !line.empty() )
-				lines.push_back( std::move( line ) );
-		}
-		lines.shrink_to_fit();
-	}
-	else
-	{
-		lines.reserve( line_count );
-		int count = 0;
-		while( std::getline( file, line ) && count < line_count )
-		{
-			if( !line.empty() )
-				lines.push_back( std::move( line ) );
-
-			count++;
-		}
+		x.push_back(xp);
+		y.push_back(yp);
+		z.push_back(zp);
+		counter++;
+		if (counter == 606) break;
 	}
 
 	file.close();
-
-	return lines;
-}
-std::string MarkerName( int num )
-{
-	return ( "marker." + std::to_string( num ) );
 }
 
-void MarkerOne( int& it )
+void getVelocities(std::vector<double> &x, std::vector<double> &y, std::vector<double> &z, int &iter)
 {
-	const auto lines = ExtractLines( "unstruc_surface_in.dat", 10 );
-
-	std::ofstream mark( MarkerName( it ) );
-	mark << MakeHeader();
-
-	for( const auto& line : lines )
+	std::ifstream file("fort.41");
+	assert(file.is_open());
+	file.seekg(16 + 606*iter);
+	int num;
+	double xp;
+	double yp;
+	double zp;
+	int counter = 0;
+	file.precision(8);
+	while (file >> num >> xp >> yp >> zp)
 	{
-		int num = 0;
-		Vec3 v;
-		std::stringstream ss;
-		ss << line;
-		ss >> num >> v;
+		x.push_back(xp);
+		y.push_back(yp);
+		z.push_back(zp);
+		counter++;
+		if (counter == 606) break;
+	}
+	iter++;
+	file.close();
+}
 
-		mark << v;
+void newMarker(std::vector<double> &x, std::vector<double> &y, std::vector<double> &z,
+	std::vector<double> vx, std::vector<double> vy, std::vector<double> vz, int iter)  
+{
+	std::ofstream mark(MarkerName(iter));
+	assert(mark.is_open());
+	mark.precision(8);
+	mark << "TITLE=\"3D TRIANGULAR SURFACE DATA\"" << std::endl;
+	mark << "VARIABLES= \"X\", \"Y\", \"Z\"" << std::endl;
+	mark << "ZONE T=\"UNSTRUC\" N= 606 E= 808 F=FEPOINT ET=TRIANGLE" << std::endl;
+	double dt = 1.0 / 200.0;
+	for (int i = 0; i < 606; i++)
+	{
+		double newx = x.at(i) + vx.at(i) * dt;
+		double newy = y.at(i) + vy.at(i) * dt;
+		double newz = z.at(i) + vz.at(i) * dt;
+
+		mark << newx << " " << newy << " " << newz << std::endl;
+
+		x.at(i) = newx;
+		y.at(i) = newy;
+		z.at(i) = newz;
+	}
+	int dnp = 202;
+	for (int i = 1; i <= dnp + 1; i++)
+	{
+		mark << i << " " << i + 1 << " " << dnp + i << std::endl;
+		mark << i + 1 << " " << dnp + i + 1 << " " << dnp + i << std::endl;
+		mark << dnp + i << " " << dnp + i + 1 << " " << 2 * dnp + i << std::endl;
+		mark << dnp + 1 + i << " " << 2 * dnp + i + 1 << " " << 2 * dnp + i << std::endl;
+		if (i + 2 > dnp)
+		{
+			mark << "1" << " " << dnp << " " << dnp * 2 << std::endl; // 1, 202, 404
+			mark << "1" << " " << dnp + 1 << " " << dnp * 2 << std::endl; // 1, 203, 404
+			mark << dnp + 1 << " " << dnp * 2 << " " << dnp * 3 << std::endl; // 203, 404, 606
+			mark << dnp + 1 << " " << dnp * 2 + 1 << " " << dnp * 3 << std::endl; // 203 405 606
+			break;
+		}
 	}
 	mark.close();
-	it++;
 }
-
-void MarkerNext( int& it )
-{
-	std::ofstream mark( MarkerName( it ) );
-	mark << MakeHeader();
-
-	const auto prevMarkLines = ExtractLines( MarkerName( it - 1 ), 114, 580 );
-	const auto fortLines = ExtractLines( "fort.41", 16, 580 );
-	
-	const double dt = 1.0f / 200.0f;
-	for( int i = 0; i < prevMarkLines.size(); ++i )
-	{
-		std::stringstream prev_ss;
-		prev_ss << prevMarkLines[ i ];
-
-		Vec3 pos;
-		prev_ss >> pos;
-		
-		std::stringstream fort_ss;
-		fort_ss << fortLines[ i ];
-
-		Vec3 vel;
-		fort_ss >> vel;
-		
-		mark << ( pos + ( vel * dt ) );
-	}
-
-	mark.close();
-
-	it++;
-}
-
 int main()
 {
-	int iteration = 1;
-	std::cout << "Working ..." << '\n';
-	MarkerOne( iteration );
-	MarkerNext( iteration );
-	std::cout << "Done!" << '\n';
-	while( !_kbhit() );
+	std::vector<double> xpos;
+	std::vector<double> ypos;
+	std::vector<double> zpos;
+
+	std::vector<double> dx;
+	std::vector<double> dy;
+	std::vector<double> dz;
+	int time = 0;
+	std::cout << "Working ..." << std::endl;
+	//getPosition(xpos, ypos, zpos);
+	//getVelocities(dx, dy, dz, time);
+	//getVelocities(dx, dy, dz, time);
+	for (int i = 0; i < 200; i++) {
+		getPosition(xpos, ypos, zpos);
+		getVelocities(dx, dy, dz, time);
+		newMarker(xpos, ypos, zpos, dx, dy, dz, time);
+	}
+	std::cout << "Done!" << std::endl;
+	while (!_kbhit());
+
 	return 0;
 }
